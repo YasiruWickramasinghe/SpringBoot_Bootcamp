f7840ae9adad610657331530896b9309bbee3fff
yasiruwickramasinghe <it19038292@my.sliit.lk>
2024-02-19 17:33:44 +0530

    Spring Core features covered

---
+++ 02_Spring_Core/07_Bean_Scopes/src/main/java/com/pearl/springcoredemo/common/TennisCoach.java
@@ -0,0 +1,16 @@
+package com.pearl.springcoredemo.common;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+    public TennisCoach(){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backhand volley.";
+    }
+}
