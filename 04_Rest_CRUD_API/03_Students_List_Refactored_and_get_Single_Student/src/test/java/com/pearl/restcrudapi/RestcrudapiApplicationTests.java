d6ecb24b61c40524347bacf13a6d1fb4ed01d2f5
yasiruwickramasinghe <it19038292@my.sliit.lk>
2024-02-21 17:13:43 +0530

    Rest API Exception Handling Completed

---
+++ 04_Rest_CRUD_API/03_Students_List_Refactored_and_get_Single_Student/src/test/java/com/pearl/restcrudapi/RestcrudapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pearl.restcrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestcrudapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
