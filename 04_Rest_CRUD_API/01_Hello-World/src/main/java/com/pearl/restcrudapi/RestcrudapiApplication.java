545445373d5d1d784391aa2c5e4fb26bc59bb889
yasiruwickramasinghe <it19038292@my.sliit.lk>
2024-02-21 13:07:10 +0530

    Rest Api created

---
+++ 04_Rest_CRUD_API/01_Hello-World/src/main/java/com/pearl/restcrudapi/RestcrudapiApplication.java
@@ -0,0 +1,13 @@
+package com.pearl.restcrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcrudapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestcrudapiApplication.class, args);
+	}
+
+}
