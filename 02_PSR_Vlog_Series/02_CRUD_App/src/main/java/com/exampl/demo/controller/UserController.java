e1fd93a28429c6b7eb61c2d447ecad99c01ffeca
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2024-08-13 17:21:00 +0530

    Backend CRUD Completed

--- 02_PSR_Vlog_Series/02_CRUD_App/src/main/java/com/exampl/demo/controller/UserController.java
+++ 02_PSR_Vlog_Series/02_CRUD_App/src/main/java/com/exampl/demo/controller/UserController.java
@@ -3,10 +3,7 @@ package com.exampl.demo.controller;
 import com.exampl.demo.dto.UserDTO;
 import com.exampl.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,8 +14,27 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    //getAllUsers API
     @GetMapping("/getUsers")
     public List<UserDTO> getUsers(){
         return userService.getAllUsers();
+    }
+
+    //createUser API
+    @PostMapping("/addUser")
+    public UserDTO saveUser(@RequestBody UserDTO userDTO){
+        return userService.saveUser(userDTO);
+    }
+
+    //updateUser API
+    @PutMapping("updateUser")
+    public UserDTO updateUser(@RequestBody UserDTO userDTO){
+        return  userService.updateUser(userDTO);
+    }
+
+    //deleteUser API - using catch userId in request parameter
+    @DeleteMapping("/deleteUser")
+    public String deleteUser(@RequestBody UserDTO userDTO){
+        return  userService.deleteUser(userDTO);
     }
 }
