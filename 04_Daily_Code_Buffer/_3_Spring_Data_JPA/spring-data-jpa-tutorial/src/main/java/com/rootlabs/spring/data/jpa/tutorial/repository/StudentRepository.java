43813f981d57929417a7802ede0b62c41ceac995
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2024-08-31 09:40:37 +0530

    JPA @Query Annotation / Native Queries / Query Named Params /  @Transactional & @Modifying Annotation

--- 04_Daily_Code_Buffer/_3_Spring_Data_JPA/spring-data-jpa-tutorial/src/main/java/com/rootlabs/spring/data/jpa/tutorial/repository/StudentRepository.java
+++ 04_Daily_Code_Buffer/_3_Spring_Data_JPA/spring-data-jpa-tutorial/src/main/java/com/rootlabs/spring/data/jpa/tutorial/repository/StudentRepository.java
@@ -2,29 +2,83 @@ package com.rootlabs.spring.data.jpa.tutorial.repository;
 
 import com.rootlabs.spring.data.jpa.tutorial.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
-    // Find Student By First Name
-    List<Student> findByFirstName(String firstName);
+    // 1) JPA -> There are in build JPA Query Methods
 
-    // Find Students By Name containing some letter
-    List<Student> findByFirstNameContaining(String name);
+    // 2) JPA Custom Query
+        //We can define Custom JPA Names Like below
 
-    // Find Student that Last Name is Not Null
-    List<Student> findByLastNameNotNull();
+        // Find Student By First Name
+        List<Student> findByFirstName(String firstName);
 
-    // Find Student By Guardian Name
-    List<Student> findByGuardianName(String guardianName);
+        // Find Students By Name containing some letter
+        List<Student> findByFirstNameContaining(String name);
 
-    // Find Student By First Name and Last Name
-    Student findByFirstNameAndLastName(String firstName, String lastName);
+        // Find Student that Last Name is Not Null
+        List<Student> findByLastNameNotNull();
+
+        // Find Student By Guardian Name
+        List<Student> findByGuardianName(String guardianName);
+
+        // Find Student By First Name and Last Name
+        Student findByFirstNameAndLastName(String firstName, String lastName);
 
 
+    // 3) JPQL
+        // Query language specific to jpa like below
+        // select s from Student s where s.emailId = ?1
 
+        //@Query Annotation
+        //Create method and Custom JPQL Query for get data
+        //this is not Native SQL query
+        //https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html
+        //In JPQL Query we write based on the class not focus on database table name or property
+
+        // Find Student By Email Address using JPQL
+        @Query("select s from Student s where s.emailId = ?1")
+        Student getStudentByEmailAddress(String emailId);
+
+        // Find Student First Name By Email Address using JPQL
+        @Query("select s.firstName from Student s where s.emailId = ?1")
+        String getStudentFirstNameByEmailAddress(String emailId);
+
+    // 4) Native SQL Query
+
+        // Find Student By Email Address using Native SQL
+        @Query(
+                value = "SELECT * FROM tbl_student s where s.email_address = ?1",
+                nativeQuery = true
+        )
+        Student getStudentByEmailAddressNativeSQL(String emailId);
+
+    // 5) Query Named Parameter
+
+        // Find Student By Email Address using Naming Parameters
+        @Query(
+                value = "SELECT * FROM tbl_student s where s.email_address = :emailId",
+                nativeQuery = true
+        )
+        Student getStudentByEmailAddressNamedParam(@Param("emailId") String emailId);
+
+    // 6) Above all queries used for FETCHING data, Here we focus on UPDATE and DELETE data
+
+        // Update Student First Name By EmailId
+        @Modifying //indicate that the query will modify the database state, This annotation is essential because, by default, @Query methods are assumed to be read-only.
+        @Transactional // ensures that the methods are executed within a transaction. If any of the operations fail (e.g., an exception is thrown), the transaction will be rolled back, maintaining data consistency.
+        @Query(
+                value = "update tbl_student set first_name = ?1 where email_address = ?2",
+                nativeQuery = true
+        )
+        int updateStudentNameByEmailId(String firstName, String emailId);
 
 }
--- 04_Daily_Code_Buffer/_3_Spring_Data_JPA/spring-data-jpa-tutorial/src/test/java/com/rootlabs/spring/data/jpa/tutorial/repository/StudentRepositoryTest.java
+++ 04_Daily_Code_Buffer/_3_Spring_Data_JPA/spring-data-jpa-tutorial/src/test/java/com/rootlabs/spring/data/jpa/tutorial/repository/StudentRepositoryTest.java
@@ -3,11 +3,15 @@ package com.rootlabs.spring.data.jpa.tutorial.repository;
 import com.rootlabs.spring.data.jpa.tutorial.entity.Guardian;
 import com.rootlabs.spring.data.jpa.tutorial.entity.Student;
 import org.junit.jupiter.api.Test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,84 +25,140 @@ class StudentRepositoryTest {
     @Autowired
     private StudentRepository studentRepository;
 
-    /*
-    // Test Case for Save Student
-    @Test
-    public void saveStudent(){
-        //Use Entity Class object  Builder Design Pattern here
-        Student student = Student.builder()
-                .emailId("yasiru@gmail.com")
-                .firstName("Yasiru")
-                .lastName("Wickramasinghe")
-                .guardianName("Jayaweera")
-                .guardianEmail("jayaweera@gmail.com")
-                .guardianMobile("0771234567")
-                .build();
+    //Enable Loggers for Debugging Errors
+    private static final Logger LOGGER = LoggerFactory.getLogger(StudentRepositoryTest.class);
 
-        //save student using jpa repository save function
-        studentRepository.save(student);
-    }
-    */
+    // 1) JPA
 
-    // Test Case for save student with guardian details
-    @Test
-    public void saveStudentWithGuardian(){
+        /*
+        // Test Case for Save Student
+        @Test
+        public void saveStudent(){
+            //Use Entity Class object  Builder Design Pattern here
+            Student student = Student.builder()
+                    .emailId("yasiru@gmail.com")
+                    .firstName("Yasiru")
+                    .lastName("Wickramasinghe")
+                    .guardianName("Jayaweera")
+                    .guardianEmail("jayaweera@gmail.com")
+                    .guardianMobile("0771234567")
+                    .build();
 
-        //create guardian obj
-        Guardian guardian = Guardian.builder()
-                .email("sarath@gmail.com")
-                .name("Sarath")
-                .mobile("0771234567")
-                .build();
+            //save student using jpa repository save function
+            studentRepository.save(student);
+        }
+        */
 
-        //create student obj
-        Student student = Student.builder()
-                .emailId("muthu@gmail.com")
-                .firstName("Muthu")
-                .lastName("Wijenayake")
-                .guardian(guardian)
-                .build();
+        // Test Case for save student with guardian details
+        @Test
+        public void saveStudentWithGuardian(){
 
-        //save student using jpa repository save function
-        studentRepository.save(student);
-    }
+            //create guardian obj
+            Guardian guardian = Guardian.builder()
+                    .email("sarath@gmail.com")
+                    .name("Sarath")
+                    .mobile("0771234567")
+                    .build();
 
-    // Test Case for Get All Student
-    @Test
-    public void printAllStudent(){
-        List<Student> studentList = studentRepository.findAll();
+            //create student obj
+            Student student = Student.builder()
+                    .emailId("muthu@gmail.com")
+                    .firstName("Muthu")
+                    .lastName("Wijenayake")
+                    .guardian(guardian)
+                    .build();
 
-        //print all student
-        System.out.println("StudentList = " + studentList);
-    }
+            //save student using jpa repository save function
+            studentRepository.save(student);
+        }
 
-    // Test Case for Custom Repository method called "findByFirstName"
-    @Test
-    public void printStudentByFirstName(){
-        List<Student> students = studentRepository.findByFirstName("Muthu");
+        // Test Case for Get All Student
+        @Test
+        public void printAllStudent(){
+            List<Student> studentList = studentRepository.findAll();
 
-        //print student
-        System.out.println("student = " + students);
-    }
+            //print all student
+            System.out.println("StudentList = " + studentList);
+        }
 
-    // Test Case for Custom Repository method called "findByFirstNameContaining"
-    @Test
-    public void printStudentByFirstNameContaining(){
-        List<Student> students = studentRepository.findByFirstNameContaining("u");
+    // 2) JPA Custom
 
-        //print student
-        System.out.println("student = " + students);
-    }
+        // Test Case for Custom Repository method called "findByFirstName"
+        @Test
+        public void printStudentByFirstName(){
+            List<Student> students = studentRepository.findByFirstName("Muthu");
 
-    // Test Case for Custom Repository method called "findByLastNameNotNull"
+            //print student
+            System.out.println("student = " + students);
+        }
+
+        // Test Case for Custom Repository method called "findByFirstNameContaining"
+        @Test
+        public void printStudentByFirstNameContaining(){
+            List<Student> students = studentRepository.findByFirstNameContaining("u");
+
+            //print student
+            System.out.println("student = " + students);
+        }
+
+        // Test Case for Custom Repository method called "findByLastNameNotNull"
 
 
-    // Test Case for Custom Repository method called "findByGuardianName"
-    @Test
-    public void printStudentBasedOnGuardianName(){
-        List<Student> students = studentRepository.findByGuardianName("Sarath");
+        // Test Case for Custom Repository method called "findByGuardianName"
+        @Test
+        public void printStudentBasedOnGuardianName(){
+            List<Student> students = studentRepository.findByGuardianName("Sarath");
 
-        System.out.println("students = " + students);
-    }
+            System.out.println("students = " + students);
+        }
+
+    // 3) Using JPQL
+
+        // Test Case for Find Student By Email Address using JPQL
+        @Test
+        public void PrintStudentByEmailAddress(){
+            Student student = studentRepository.getStudentByEmailAddress("yasiru@gmail.com");
+
+            LOGGER.info("Student = {}", student);
+        }
+
+        // Test Case for Find Student First Name By Email Address using JPQL
+        @Test
+        public void PrintStudentFirstNameByEmailAddress(){
+            String firstName = studentRepository.getStudentFirstNameByEmailAddress("muthu@gmail.com");
+
+            LOGGER.info("Student First Name = {}", firstName );
+        }
+
+    // 4) Using Native SQL
+
+        // Test Case for Find Student By Email Address using Native SQL
+        @Test
+        public void PrintStudentByEmailAddressNativeSQL(){
+            Student student = studentRepository.getStudentByEmailAddressNativeSQL("muthu@gmail.com");
+
+            LOGGER.info("Student = {}", student);
+        }
+
+    // 5) Using Named Parameter
+
+        // Test Case for Find Student By Email Address using Named Parameter
+        @Test
+        public void PrintStudentByEmailAddressNamedParam(){
+            Student student = studentRepository.getStudentByEmailAddressNamedParam("yasiru@gmail.com");
+
+            LOGGER.info("Student = {}", student);
+        }
+
+    // 6) Above all queries used for FETCHING data, Here we focus on UPDATE and DELETE data
+
+        // Update Student First Name By EmailId
+        @Test
+        public void updateStudentNameByEmailId(){
+            studentRepository.updateStudentNameByEmailId(
+                    "Yash",
+                    "yasiru@gmail.com"
+            );
+        }
 
 }
