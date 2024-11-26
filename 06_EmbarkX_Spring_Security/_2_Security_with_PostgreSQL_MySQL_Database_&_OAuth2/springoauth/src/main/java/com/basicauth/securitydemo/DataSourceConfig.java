1304fac269148d8c67b3775a0133249e92bcb03f
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2024-11-26 16:15:57 +0530

    postgresql authentication

---
+++ 06_EmbarkX_Spring_Security/_2_Security_with_PostgreSQL_MySQL_Database_&_OAuth2/springoauth/src/main/java/com/basicauth/securitydemo/DataSourceConfig.java
@@ -0,0 +1,27 @@
+package com.basicauth.securitydemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public  DataSourceInitializer dataSourceInitializer(){
+        DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("schema.sql"));
+        initializer.setDatabasePopulator(populator);
+        return initializer;
+    }
+}
---
+++ 06_EmbarkX_Spring_Security/_2_Security_with_PostgreSQL_MySQL_Database_&_OAuth2/springoauth/src/main/resources/schema.sql
@@ -0,0 +1,20 @@
+--create table users(username varchar_ignorecase(50) not null primary key,password varchar_ignorecase(500) not null,enabled boolean not null);
+--create table authorities (username varchar_ignorecase(50) not null,authority varchar_ignorecase(50) not null,constraint fk_authorities_users foreign key(username) references users(username));
+--create unique index ix_auth_username on authorities (username,authority);
+
+DROP TABLE IF EXISTS authorities;
+DROP TABLE IF EXISTS users;
+
+create table users (
+    username varchar(50) not null primary key,
+    password varchar(500) not null,
+    enabled boolean not null
+);
+
+create table authorities (
+    username varchar(50) not null,
+    authority varchar(50) not null,
+    constraint fk_authorities_users foreign key(username) references users(username)
+);
+
+create unique index ix_auth_username on authorities (username, authority);
