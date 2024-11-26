4ab73401620d0011d36a53381aa57316e8d07349
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2024-11-26 10:40:00 +0530

    In memory authentication and database authentiocation with role based authirization and hashing the password

--- 06_EmbarkX_Spring_Security/_1_Security_with_JWT_Authentication/1_securitydemo/securitydemo/src/main/java/com/basicauth/securitydemo/GreetingsController.java
+++ 06_EmbarkX_Spring_Security/_1_Security_with_JWT_Authentication/1_securitydemo/securitydemo/src/main/java/com/basicauth/securitydemo/GreetingsController.java
@@ -1,5 +1,6 @@
 package com.basicauth.securitydemo;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,19 @@ public class GreetingsController {
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello";
+    }
+
+    //Role Based Authorization
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndpoint(){
+        return "Hello User!";
+    }
+
+    //Role Based Authorization
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndpoint(){
+        return "Hello Admin!";
     }
 }
--- 06_EmbarkX_Spring_Security/_1_Security_with_JWT_Authentication/1_securitydemo/securitydemo/src/main/java/com/basicauth/securitydemo/SecurityConfig.java
+++ 06_EmbarkX_Spring_Security/_1_Security_with_JWT_Authentication/1_securitydemo/securitydemo/src/main/java/com/basicauth/securitydemo/SecurityConfig.java
@@ -1,27 +1,83 @@
 package com.basicauth.securitydemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
+    //make use datasource
+    @Autowired
+    DataSource dataSource;
+
+    // Set Basic Authentication for Project
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.authorizeHttpRequests((requests) ->
+                requests.requestMatchers("/h2-console/**").permitAll() // here if get this url (/h2-console) permit the authentication
+                .anyRequest().authenticated());
 
         //Remove Cookies and make Stateless API
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         
         //http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
+
+        http.headers(headers ->
+                headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
+
+    // in memory authentication for user management
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        //Creating new In memory User
+        // below {noop} means password stored as clean password not encoded
+        UserDetails user1 = User.withUsername("user1")
+                .password(passwordEncoder().encode("password1"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("adminPassword"))
+                .roles("ADMIN")
+                .build();
+
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.createUser(user1);
+        userDetailsManager.createUser(admin);
+        return userDetailsManager;
+
+        //return new InMemoryUserDetailsManager(user1, admin);
+    }
+
+    // Get bean of Hashing the Password
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }
