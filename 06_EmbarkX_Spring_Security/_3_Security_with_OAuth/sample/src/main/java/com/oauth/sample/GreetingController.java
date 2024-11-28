481bcfd088ca1ece3a043a6b8251cc64e32d2f21
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2024-11-28 08:51:35 +0530

    Github Oauth2 Configure complete

---
+++ 06_EmbarkX_Spring_Security/_3_Security_with_OAuth/sample/src/main/java/com/oauth/sample/GreetingController.java
@@ -0,0 +1,13 @@
+package com.oauth.sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/hello")
+    public String  sayHello(){
+        return "Hello";
+    }
+}
